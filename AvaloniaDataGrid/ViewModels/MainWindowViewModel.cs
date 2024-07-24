928df89478bb0cb14d8dc045d02478446134f46a
vlas <vlas@HOME-PC>
2024-07-24 17:36:26 +0300

    не работает галочка

--- AvaloniaDataGrid/ViewModels/MainWindowViewModel.cs
+++ AvaloniaDataGrid/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.ObjectModel;
 using System;
 using System.Collections.Generic;
+using Spreadalonia;
+using ReactiveUI;
+
 
 namespace AvaloniaDataGrid.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-
+        public TableData TableDataSpread { get; set; } = new TableData();
         public ObservableCollection<Person> People { get; }
         public MainWindowViewModel()
         {
@@ -21,6 +24,27 @@ namespace AvaloniaDataGrid.ViewModels
                 people.Add(new Person(part, false));
             }
             People = new ObservableCollection<Person>(people);
+            
+            
         }
+        public void ButtonClick() 
+        {
+            int i = 0;
+            var viewModel = new MainWindowViewModel();
+            var substanceList = new List<string>();
+            foreach (var people in viewModel.People)
+            {
+                if (people.IsChoosen == true)
+                {
+                    substanceList.Add(people.Substance);
+                    TableDataSpread.HorizontalHeaders.Add(people.Substance);
+
+                    TableDataSpread.Values[(0, i)] = people.Substance;
+                    TableDataSpread.CellStates[(0, i)] = CellState.ReadOnly;
+                    this.RaisePropertyChanged(nameof(TableDataSpread));
+                    i++;
+                }
+            }
+        }    
     }
 }
